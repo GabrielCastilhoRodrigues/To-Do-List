1eb90f314c66b382a0de8f20ffa0de98028bf114
Gabriel Castilho Rodrigues <gabrielrcastilho@outlook.com>
2024-03-09 03:23:56 -0300

    Criação da comunicação API
    - Criado API para comunicação com o banco de dados
    do projeto.

---
+++ nodejs-backend-api/index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+ 
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json()); 
+app.get('/', (req, res) => 
+    res.json({ 
+        message: 'Funcionando!' 
+    }));
+
+//inicia o servidor
+app.listen(port);
+console.log('API funcionando!');
